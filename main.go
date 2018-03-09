5b1710f7f65f15c79e4f6e4a7d3fe5634741ee73
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-03-09 15:00:57 +0530

    Add database handling methods

--- main.go
+++ main.go
@@ -3,9 +3,12 @@
 package main
 
 import (
+	"github.com/jarifibrahim/todo/app"
+
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
-	"github.com/jarifibrahim/todo/app"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func main() {
@@ -22,8 +25,22 @@ func main() {
 	c := NewTodoController(service)
 	app.MountTodoController(service, c)
 
+	// Initialize Database
+	initDB()
+
 	// Start service
 	if err := service.ListenAndServe(":8080"); err != nil {
 		service.LogError("startup", "err", err)
 	}
+}
+
+func initDB() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Todo{})
 }
---
+++ repository.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Todo struct {
+	gorm.Model
+	Title       string
+	Description string
+}
+
+type todoRepository struct{}
+
+// TableName overrides the table name settings in Gorm to force a specific table name
+// in the database.
+func (p Todo) TableName() string {
+	return "todos"
+}
+
+func (t *todoRepository) Create(ctx context.Context, todo *Todo) (*Todo, error) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	if err := db.Create(todo).Error; err != nil {
+		return nil, err
+	}
+	return todo, nil
+}
+
+func (t *todoRepository) Delete(ctx context.Context, ID uint) error {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		return errors.New("Failed to connect to database")
+	}
+	defer db.Close()
+
+	var tempTodo Todo
+	if err := db.First(&tempTodo, ID).Error; err != nil {
+		return errors.New("Failed to fetch item from database")
+	}
+	if err := db.Delete(&tempTodo).Error; err != nil {
+		return errors.New("Failed to delete item from database")
+	}
+	return nil
+}
+
+func (t *todoRepository) List(ctx context.Context) ([]Todo, error) {
+	return nil, nil
+}
+
+func (t *todoRepository) Show(ctx context.Context, ID uint) (*Todo, error) {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		return nil, errors.New("Failed to connect to database")
+	}
+	defer db.Close()
+
+	var tempTodo Todo
+	if err := db.First(&tempTodo, ID).Error; err != nil {
+		return nil, errors.New("Failed to fetch item from database")
+	}
+	return &tempTodo, nil
+}
